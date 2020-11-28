25676b0ef84fd75feefcfb9731bf532edf44d29a
“ethan-jian <729381317@qq.com>
2020-11-28 20:10:55 +0800

    增加登录页面

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,48 @@
 import request from '@/utils/request'
+import {deleteRequest, getRequest, postRequest} from "@/api/common";
 
-export function login(data) {
-  return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
+export const login = function (username, password) {
+  return postRequest('user/login/', {
+      username: username,
+      password:password,
   })
-}
+};
 
-export function getInfo(token) {
-  return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
+export const register = function (username, password) {
+  return postRequest('user/register/', {
+      username: username,
+      password:password,
   })
-}
+};
 
-export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
-}
+export const logout = function () {
+  return deleteRequest('user/logout')
+};
+
+
+export const getLoginUserInfo = function () {
+  return getRequest('user/info')
+};
+
+// export function login(data) {
+//   return request({
+//     url: '/vue-admin-template/user/login',
+//     method: 'post',
+//     data
+//   })
+// }
+//
+// export function getInfo(token) {
+//   return request({
+//     url: '/vue-admin-template/user/info',
+//     method: 'get',
+//     params: { token }
+//   })
+// }
+//
+// export function logout() {
+//   return request({
+//     url: '/vue-admin-template/user/logout',
+//     method: 'post'
+//   })
+// }
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-row class="tac">
+    <el-col :span="12">
+      <h5>默认颜色</h5>
+      <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">项目管理</span>
+        </el-menu-item>
+        <el-menu-item index="2" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">接口信息</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-setting"></i>
+          <span slot="title">接口用例</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">报告管理</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default {
+    name: "home",
+    data() {
+      return {}
+    },
+    methods: {
+      created() {
+
+      },
+      watch: {}
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
