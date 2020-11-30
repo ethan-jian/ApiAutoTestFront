572badcb1a2c3c4aff8e21429bb8cf18f22129b5
ethan-jian <729381317@qq.com>
2020-12-01 00:53:51 +0800

    增加右上角登出

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="home">
+        <div class="dropdown">
+            <el-dropdown>
+  <span class="el-dropdown-link">
+      用户<i class="el-icon-arrow-down el-icon--right"></i>
+
+  </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item @click="userInfo">用户信息</el-dropdown-item>
+                    <el-dropdown-item @click="userLogout">登出</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </div>
         <el-row class="tac">
             <el-col :span="12">
                 <el-menu
@@ -9,13 +21,14 @@
                     <el-menu-item index="1">
                         <i class="el-icon-menu"></i>
                         <span slot="title">项目管理</span>
+
                     </el-menu-item>
                     <el-menu-item index="2">
                         <i class="el-icon-document"></i>
                         <span slot="title">接口信息</span>
                     </el-menu-item>
                     <el-menu-item index="3">
-                        <i class="el-icon-setting"></i>
+                        <i class="el-icon-document"></i>
                         <span slot="title">接口用例</span>
                     </el-menu-item>
                     <el-menu-item index="4">
@@ -29,12 +42,20 @@
 </template>
 
 <script>
+    import {getLoginUserInfo, logout} from "../api/user";
+
     export default {
         name: "home",
         data() {
             return {}
         },
         methods: {
+            userInfo(){
+
+            },
+            userLogout(){
+                logout()
+            },
             created() {
 
             },
@@ -47,6 +68,27 @@
 <style scoped>
     .home {
         width: 20px;
+    }
+
+    .el-dropdown-link {
+        cursor: pointer;
+        color: #409EFF;
+        float: right;
+
+    }
+
+    .el-icon-arrow-down {
+        font-size: 12px;
+    }
+
+    .dropdown {
+        position: absolute;
+        width: 10px;
+        height: 10px;
+        right: 5%;
+        top: 0;
+
+        /*z-index: 100;*/
     }
 
 
