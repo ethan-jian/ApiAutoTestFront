f97f566b50c794c50386bd84fa3976ae3c0afac2
“ethan-jian <729381317@qq.com>
2020-11-30 18:00:03 +0800

    优化登录成功失败消息提示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,53 @@
 <template>
-  <el-row class="tac">
-    <el-col :span="12">
-      <h5>默认颜色</h5>
-      <el-menu
-        default-active="1"
-        class="el-menu-vertical-demo"
-      >
-        <el-menu-item index="1">
-          <i class="el-icon-menu"></i>
-          <span slot="title">项目管理</span>
-        </el-menu-item>
-        <el-menu-item index="2" disabled>
-          <i class="el-icon-document"></i>
-          <span slot="title">接口信息</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <i class="el-icon-setting"></i>
-          <span slot="title">接口用例</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">报告管理</span>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-  </el-row>
+    <div class="home">
+        <el-row class="tac">
+            <el-col :span="12">
+                <el-menu
+                        default-active="1"
+                        class="el-menu-vertical-demo"
+                >
+                    <el-menu-item index="1">
+                        <i class="el-icon-menu"></i>
+                        <span slot="title">项目管理</span>
+                    </el-menu-item>
+                    <el-menu-item index="2">
+                        <i class="el-icon-document"></i>
+                        <span slot="title">接口信息</span>
+                    </el-menu-item>
+                    <el-menu-item index="3">
+                        <i class="el-icon-setting"></i>
+                        <span slot="title">接口用例</span>
+                    </el-menu-item>
+                    <el-menu-item index="4">
+                        <i class="el-icon-setting"></i>
+                        <span slot="title">报告管理</span>
+                    </el-menu-item>
+                </el-menu>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: "home",
-    data() {
-      return {}
-    },
-    methods: {
-      created() {
+    export default {
+        name: "home",
+        data() {
+            return {}
+        },
+        methods: {
+            created() {
 
-      },
-      watch: {}
+            },
+            watch: {}
+        }
+
     }
-
-  }
 </script>
 
 <style scoped>
+    .home {
+        width: 20px;
+    }
+
 
 </style>
